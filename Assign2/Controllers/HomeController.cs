b6e03ff42e9a4f9af1005286bb207093f45b25ee
stephens-bryan <bryanstephensdevelopment@gmail.com>
2017-02-07 19:14:09 -0500

    My solution to Assignment 2

---
+++ Assign2/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Assign2.Models;
+
+namespace Assign2.Controllers
+{
+    public class HomeController : Controller
+    {
+        PatientContext patientContext = new PatientContext();
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<Patient> patients = patientContext.Patients.ToList();
+            return View(patients);
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id.HasValue)
+            {
+                Patient patient = patientContext.Patients.Single(p => p.Id == id);
+                return View(patient);
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
---
+++ Assign2/Models/Patient.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+//using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Assign2.Models
+{
+    [System.ComponentModel.DataAnnotations.Schema.Table("Table")]
+    public class Patient
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "First Name")]
+        public string firstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string lastName { get; set; }
+
+        [Display(Name = "Health Card #")]
+        public string healthCarNum { get; set; }
+
+        [Display(Name = "Date of Birth")]
+        [DisplayFormat(DataFormatString = "{0: MMMM dd, yyyy}")]
+        public DateTime birthDate { get; set; }
+
+        // concat firstName & lastName together
+        public string fullName()
+        {
+            return this.firstName + " " + this.lastName;
+        }
+        
+    }
+
+    public class PatientContext : DbContext
+    {
+        public DbSet<Patient> Patients { get; set; }
+    }
+}
